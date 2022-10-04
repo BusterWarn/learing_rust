f376235fa6a3334d6e2d063e1c471079408fc29f
Buster Hultgren Wärn <buster.hultgrenwarn@tietoevry.com>
2022-10-04 17:40:31 +0200

    Fixed hashmap return

--- hello_world/fibonacci.rs
+++ hello_world/fibonacci.rs
@@ -2,28 +2,39 @@ use std::collections::HashMap;
 
 fn main()
 {
-  let memoization:HashMap<i32, i32> = HashMap::new();
-  let my_int = fibonacci(12, &memoization);
+  let mut memoization:HashMap<i32, i32> = HashMap::new();
+  let my_int = fibonacci(12, &mut memoization);
 
   println!("Fibonacci: {}", my_int);
 }
 
-fn fibonacci(n: i32, mem: &HashMap<i32, i32>) -> &i32
+fn fibonacci(n: i32, mem: &mut HashMap<i32, i32>) -> i32
 {
   if n <= 2
   {
-    return &1;
+    return 1;
   }
 
-  //let mut return_value:i32 = 0;
+  match mem.get(&n) {
+    Some(res) => {
+      return *res;
+    },
+    None => {
+      let fib_1 = fibonacci(n - 1, mem);
+      let fib_2 = fibonacci(n  - 2, mem);
+      mem.insert(n,  fib_1 + fib_2);
+      return fib_1 + fib_2;
+    }
+  }
+/*
   if mem.contains_key(&n)
   {
-    //return_value = 
-    return mem.get(&n).unwrap();
+    return mem.get(&n).unwrap().clone();
   }
 
-  mem.insert(n, fibonacci(n - 1, &mem) + fibonacci(n  - 2, &mem));
+  let fib_1 = fibonacci(n - 1, mem);
+  let fib_2 = fibonacci(n  - 2, mem);
+  mem.insert(n,  fib_1 + fib_2);
 
-  return &5;
-  // return *mem.get(&n).unwrap_or(&0);
+  return *mem.get(&n).unwrap_or(0).clone(); */
 }
