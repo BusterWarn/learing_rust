d435376f4b6fbbd3214e08266145984af3cc6f18
Buster Hultgren Wärn <buster.hultgrenwarn@tietoevry.com>
2022-10-09 13:03:59 +0200

    Try to compile Rust into C++ library

---
+++ c_with_rust/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "lib.h"
+
+int main()
+{
+  std::cout << "Hello World " << rust_function() << "\n";
+
+  return 0;
+}
---
+++ c_with_rust/rust_lib/include/lib.h
@@ -0,0 +1,6 @@
+#ifndef __CUSTOM_RUST_LIB__
+#define __CUSTOM_RUST_LIB__
+
+[[nodiscard]] std::uint32_t rust_function();
+
+#endif // __CUSTOM_RUST_LIB__
